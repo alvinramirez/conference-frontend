b3007400b7a018b05b79753c07a400bab515fb06
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-05-11 00:04:01 +0200

    Changed entire app.js file for fetching my api and rendering the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    // Constructor
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+
+    // ComponentDidMount is used to
+    // execute the code
+    componentDidMount() {
+        fetch("http://localhost:8080/api/v1/user/hello")
+        .then((res) => res.json())
+        .then((json) => {
+            this.setState({
+                items: json,
+                DataisLoaded: true
+            });
+        })
+    }
+
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) 
+        return <div>
+                    <h1> Please wait... </h1>
+
+               </div>;
+        return (
+        <div className="App">
+            <h1> Fetch data from an api in React </h1>
+                <ol key = { items.id }>
+                    Full Name: { items.name }
+                </ol>
+        </div>
+        );
+    }
 }
 
 export default App;
